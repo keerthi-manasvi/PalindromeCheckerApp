87f238be655f3f20d648a79506de050ada383814
keerthi-manasvi <manjumanasvi.k@gmail.com>
2026-03-12 15:11:38 +0530

    UC8

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,33 +1,79 @@
-import java.util.*;
+class Node {
+    char data;
+    Node next;
 
-public class PalindromeCheckerApp {
-    public static boolean isPalindrome(String str) {
-        Deque<Character> deque = new LinkedList<>();
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class PalindromeCheckerApp{
+    // Convert string to linked list
+    public static Node convertToLinkedList(String str) {
+        Node head = null, tail = null;
         for (char ch : str.toCharArray()) {
-            deque.addLast(ch);
+            Node newNode = new Node(ch);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
         }
-        while (deque.size() > 1) {
-            if (deque.removeFirst() != deque.removeLast()) {
+        return head;
+    }
+
+    private static Node reverse(Node head) {
+        Node prev = null, curr = head, next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static boolean isPalindrome(Node head) {
+        if (head == null || head.next == null) return true;
+
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node secondHalf = reverse(slow);
+
+        Node firstHalf = head;
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
                 return false;
             }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
         }
         return true;
     }
+
     public static void main(String[] args) {
-        String input = "radar";
-        if (isPalindrome(input)) {
+        String input = "level";
+        Node head = convertToLinkedList(input);
+
+        if (isPalindrome(head)) {
             System.out.println(input + " is a palindrome.");
         } else {
             System.out.println(input + " is not a palindrome.");
         }
+        String input1 = "lower";
+        Node head1 = convertToLinkedList(input1);
 
-        String input1 = "random";
-        if (isPalindrome(input1)) {
+        if (isPalindrome(head1)) {
             System.out.println(input1 + " is a palindrome.");
         } else {
             System.out.println(input1 + " is not a palindrome.");
         }
     }
 }
-
-
